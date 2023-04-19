987a68a1a0c52f19c87716faed4eeff65b94e6b1
paulanik <19-39880-1@student.aiub.edu>
2023-04-19 20:51:54 +0600

    update

--- lib/common/values/server.dart
+++ lib/common/values/server.dart
@@ -12,6 +12,7 @@
  const SERVER_API_URL = 'http://192.168.31.148:8000/';
 //  const SERVER_API_URL = 'http://192.168.189.89:8000/';
 //  const SERVER_API_URL = 'http://192.168.201.167:8000/';
+//  const SERVER_API_URL = 'https://paulanik.github.io/';
 
  // ignore: constant_identifier_names
 //  const APPID = "e11666dd3c4346568b67b80823bcca50";
